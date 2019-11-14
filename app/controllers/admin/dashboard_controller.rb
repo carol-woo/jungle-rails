0c9f8ceb8c15fd4c0118d071d577f55f43315238
Araxiah <cewoo98@hotmail.com>
2019-11-13 19:32:22 -0500

    fixed dashboard on the admin side

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
 
   def show
+    @products = Product.all.length
+    @category = Category.all.length
   end
+
 end
